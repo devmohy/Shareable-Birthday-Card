3ea908b6d4d17206d1e226459afc26179eeffa2b
Shuhei Shagawa <sgw.sgw.shu@gmail.com>
2018-04-22 22:52:38 -0700

    added css animation

--- assets/js/cake.js
+++ assets/js/cake.js
@@ -4,7 +4,4 @@ window.onload = function(){
   var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October ", "November",  "December"][today.getMonth()];
   var day = today.getDate();
   document.getElementById('date').innerHTML = year + '/' + month + '/' + day + "th";
-
-  // 
 }
- 
