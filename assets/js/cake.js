5686a7a703c9be4dc099af9eabab097c71b79440
Shuhei Shagawa <sgw.sgw.shu@gmail.com>
2018-04-23 19:58:40 -0700

    Top page and btn added

--- assets/js/cake.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-  var today = new Date();
-  var year = today.getFullYear();
-  var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October ", "November",  "December"][today.getMonth()];
-  var day = today.getDate();
-  document.getElementById('date').innerHTML = year + '/' + month + '/' + day + "th";
-}
--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,18 @@ window.onload = function(){
   var day = today.getDate();
   document.getElementById('date').innerHTML = year + '/' + month + '/' + day + "th";
 
-  // 
+
+  $("#balloon").click(function(){
+    $(this).animate({top: '-50%'}, {duration: 2000});
+  });
+  $("#btn").click(function(){
+    $(this).siblings(".balloones")
+    .animate({
+    'top':'-35%',
+    'opacity':'1'
+    },5000)
+    setTimeout(function(){
+        $("#cover").animate({opacity:'0'},1000);
+    },3000);
+  });
 }
- 
