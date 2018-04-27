b9766e60f93e2bdeb446103138c1d12941726f63
Shuhei Shagawa <sgw.sgw.shu@gmail.com>
2018-04-27 00:25:13 -0700

    ajax added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+//Date
 window.onload = function(){
   var today = new Date();
   var year = today.getFullYear();
@@ -5,7 +6,7 @@ window.onload = function(){
   var day = today.getDate();
   document.getElementById('date').innerHTML = year + '/' + month + '/' + day + "th";
 
-
+// balloon
   $("#balloon").click(function(){
     $(this).animate({top: '-50%'}, {duration: 2000});
   });
@@ -15,4 +16,62 @@ window.onload = function(){
         $("#btn").parent("#cover").hide();
     },1000);
   });
+
+// ajax to get itunes music
+     var params = {
+        lang: 'ja_jp',
+        entry: 'music',
+        media: 'music',
+        country: 'JP',
+        term: keyWord,
+        limit: maxNum,
+
+      };
+
+var keyWord = "sample",
+    maxNum = "sample2"
+
+    $(".note").click(function(keyWord, maxNum){
+
+      var msg="birthday"; //検索ワード
+      var params = {
+      term: msg,
+      limit: 30, //データ数の上限
+      lang: "ja_jp",
+      entry: "music",
+      media: "music",
+      country: "US",
+      };
+
+      $.ajax({
+        url: "https:itunes.apple.com/search",
+        method: "GET",
+        data: params,
+        dataType: "jsonp",
+
+      success: function(json){
+        console.log(json);
+        srcUrl = new Array(json.results.length);
+        // srcTitle = new Array(json.results.length);
+        // srcArt = new Array(json.results.length);
+        // srcArtist = new Array(json.results.length);
+        console.log(json.results[0].previewUrl);
+
+        // for (var i=0;i<json.results.length;i++) {
+        //   // srcUrl[i] = json.results[i].previewUrl; //preview
+        //   // srcTitle[i] = json.results[i].trackName; //SNG name
+        //   // srcArt[i] = json.results[i].artworkUrl100; //img
+        //   // srcArtist[i] = json.results[i].artistName; //AT name
+        //   // console.log(srcUrl[i]);
+        // }
+
+      }
+
+      // error: function() {
+      //   console.log("itunes api search error.", arguments);
+      // },
+    });
+
+  });
+
 }
